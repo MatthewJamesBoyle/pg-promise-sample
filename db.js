36e46a704d9316d9847d26426e1043dd89ba9c42
Matthew Boyle <matthewboyle@Matthews-MacBook-Pro.local>
2021-01-25 13:47:13 +0000

    singleton in vanilla js

--- db.js
+++ db.js
@@ -8,22 +8,25 @@ const host = process.env.DB_HOST
 const port = process.env.DB_PORT
 const database = process.env.DB_NAME
 
-const db = pgp(`postgres://${user}:${password}@${host}:${port}/${database}`);
+const dbConn = pgp(`postgres://${user}:${password}@${host}:${port}/${database}`);
 
-function getSingleton(name, defaults) {
-    const s = Symbol.for(name); // get global symbol
-    let scope = global[s]; // get scope from the global
+
+function createSingleton(name, create) {
+    let s = Symbol.for(name);
+    let scope = global[s];
     if (!scope) {
-        scope = {...defaults || {}}; // create singleton scope, with optional defaults
-        global[s] = scope; // save our singleton in the global scope
+        scope = {...create()};
+        global[s] = scope;
     }
     return scope;
 }
 
-function getDB() {
-    // get singleton, set db and pgp in it, if for the first time:
-    return getSingleton('my-app-singleton-space', {db, pgp});
+export function getDB() {
+    return createSingleton('my-app-db-space', () => {
+        return {
+            db: dbConn
+        };
+    });
 }
-
 
 module.exports = {getDB}
